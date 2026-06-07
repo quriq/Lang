55a37aadcf78e2323ccb7bf58923cd6f03890577
quriqyl <krysanv-k@mail.ru>
2026-06-07 04:39:05 +0300

    first commit

---
+++ src/test/java/com/example/lang/LangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
