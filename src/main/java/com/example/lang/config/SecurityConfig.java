9e2f7ee4e905c9a801c0282b2500c4bb0aeec7ef
quriqyl <krysanv-k@mail.ru>
2026-07-01 02:14:37 +0300

    security config

--- src/main/java/com/example/lang/config/SecurityConfig.java
+++ src/main/java/com/example/lang/config/SecurityConfig.java
@@ -1,4 +1,47 @@
 package com.example.lang.config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private DataSource dataSource;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/reg").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll())
+                .userDetailsService(users(dataSource));
+        return http.build();
+    }
+    @Bean
+    public UserDetailsService users(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select login, psw, enabled from users where login=?");
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select login, 'USER' from users where login=?");
+        return jdbcUserDetailsManager;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
+
--- src/main/java/com/example/lang/config/WebConfig.java
+++ src/main/java/com/example/lang/config/WebConfig.java
@@ -1,4 +1,5 @@
 package com.example.lang.config;
 
 public class WebConfig {
+    
 }
