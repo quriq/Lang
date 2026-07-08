934c85b85eaf744c2503845f6d598ba6ee80125a
quriqyl <krysanov-k@mail.ru>
2026-07-09 02:58:21 +0300

    repo

---
+++ src/main/java/com/example/lang/repository/FolderRepository.java
@@ -0,0 +1,11 @@
+package com.example.lang.repository;
+
+import com.example.lang.entity.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface FolderRepository extends JpaRepository<Folder, Long> {
+    List<Folder> findByUserId(Long userId);
+}
