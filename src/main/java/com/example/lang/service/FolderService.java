3cec1a53a712f86c05369ba4aad4bbf6a1e0d8d6
quriqyl <krysanov-k@mail.ru>
2026-07-09 03:18:30 +0300

    folderService

---
+++ src/main/java/com/example/lang/service/FolderService.java
@@ -0,0 +1,31 @@
+package com.example.lang.service;
+
+import com.example.lang.entity.Folder;
+import com.example.lang.repository.FolderRepository;
+import com.example.lang.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class FolderService {
+    @Autowired
+    private FolderRepository folderRepository;
+
+    public Folder createFolder(String name, User owner){
+        if(name == null || name.trim().isEmpty()){
+            throw new IllegalArgumentException("Название папки не может быть пустым");
+        }
+        Folder folder = new Folder();
+        folder.setName(name);
+
+        folder.setUser(owner);
+        folder.setCreatedAt(LocalDateTime.now());
+
+        return folderRepository.save(folder);
+
+    }
+    public List<Folder> getFoldersByUser(User user){return folderRepository.findByUserId(user.getId());}
+}
